8ec13c295bd293b80810f07e74e3bd48d9501aae
Vinicius Monteiro Arjonas <viniciusmonteiroarjonas@outlook.com>
2022-04-12 17:55:16 -0300

    first commit

---
+++ src/test/java/br/com/springKafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,9 @@
+package br.com.springKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+}
