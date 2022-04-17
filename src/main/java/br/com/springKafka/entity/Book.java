95c05cb665fbbca085a0c6722dc7e65569e584a5
Vinicius Monteiro Arjonas <viniciusmonteiroarjonas@outlook.com>
2022-04-17 14:12:40 -0300

    criado entidades

---
+++ src/main/java/br/com/springKafka/entity/Book.java
@@ -0,0 +1,24 @@
+package br.com.springKafka.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+
+    @ManyToOne
+    private People people;
+
+
+}
